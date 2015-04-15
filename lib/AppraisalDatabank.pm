c34c9495696c54252b9eea4185137e2b346ad79f
lstuckey <lstuck427@gmail.com>
2015-04-14 22:06:04 -0500

    add new font as asset

--- lib/AppraisalDatabank.pm
+++ lib/AppraisalDatabank.pm
@@ -65,7 +65,7 @@ sub _add_assets {
 
   $app->asset('adb.css' =>
     'http://cdnjs.cloudflare.com/ajax/libs/pure/0.6.0/pure-min.css',
-    'http://fonts.googleapis.com/css?family=EB+Garamond',
+    'http://fonts.googleapis.com/css?family=Tinos:400,700,400italic',
     'https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css',
     '/sass/adb.scss',
     '/css/adb.css',
