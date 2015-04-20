079da1ddbf7b08ecdd7e346374ab35e3182836b5
lstuckey <lstuck427@gmail.com>
2015-04-20 16:43:43 -0500

    Add dropdown search form

--- public/js/adb.js
+++ public/js/adb.js
@@ -18,3 +18,24 @@ $(function() {
         }
     });
 });
+
+//Dropdown search menu
+$(function() {
+    var button = $('#searchButton');
+    var box = $('#searchBox');
+    var form = $('#searchForm');
+    button.removeAttr('href');
+    button.mouseup(function(login) {
+        box.toggle();
+        button.toggleClass('active');
+    });
+    form.mouseup(function() { 
+        return false;
+    });
+    $(this).mouseup(function(login) {
+        if(!($(login.target).parent('#searchButton').length > 0)) {
+            button.removeClass('active');
+            box.hide();
+        }
+    });
+});
