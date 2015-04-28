b36fe1a790867adcbf2cb96e803d13e8d8828a2f
lstuckey <lstuck427@gmail.com>
2015-04-28 18:19:08 -0500

    update jQuery

--- public/js/adb.js
+++ public/js/adb.js
@@ -1,62 +1,59 @@
+$(document).ready(function(){
+  
 //Dropdown login menu
-$(function() {
-    var button = $('#loginButton');
-    var box = $('#loginBox');
-    var form = $('#loginForm');
-    button.removeAttr('href');
-    button.mouseup(function(login) {
-        box.toggle();
-        button.toggleClass('active');
-    });
-    form.mouseup(function() { 
-        return false;
-    });
-    $(this).mouseup(function(login) {
-        if(!($(login.target).parent('#loginButton').length > 0)) {
-            button.removeClass('active');
-            box.hide();
-        }
-    });
+var button = $('#loginButton');
+var box = $('#loginBox');
+var form = $('#loginForm');
+button.removeAttr('href');
+button.mouseup(function(login) {
+    box.toggle();
+    button.toggleClass('active');
 });
+form.mouseup(function() { 
+    return false;
+});
+$(this).mouseup(function(login) {
+    if(!($(login.target).parent('#loginButton').length > 0)) {
+        button.removeClass('active');
+        box.hide();
+    }
+});
+
 
 //Toggle search form from Revise Search button and "search" in menu
-$(function() {
-    var button = $('#search-button');
-    $('#reviseSearch').show();
-    $('.search-form').hide();
-    button.removeAttr('href');
-    button.css('cursor', 'pointer');
+var button = $('#search-button');
+var form = $('#search-form');
+var revise = $('#reviseSearch');
+    
+revise.show();
+form.hide();
+button.removeAttr('href');
+button.css('cursor', 'pointer');
+    
+revise.click(function() {
+    button.addClass('active');
+    form.toggle();
+});
+button.click(function(){
+    $(this).addClass('active');
+    form.toggle();
 })
 
-$(function() {
-    var button = $('#search-button');
-    var form = $('#search-form');
-    $('#reviseSearch').click(function() {
-        button.toggleClass('active');
-        form.toggle();
-    });
-    button.click(function(){
-        $(this).toggleClass('active');
-        form.toggle();
-    })
-});
 
 //Registration Form lightbox
-var $overlay = $('<div id="overlay"></div>');
-$(function() {
-    $("body").append($overlay);
-    $overlay.append($('#regForm'));
+var overlay = $('<div id="overlay"></div>');
+var link = $('#regLink');
+var regForm = $('#regForm');
+    
+    
+$("body").append(overlay);
+overlay.append(regForm);
+
+link.removeAttr('href');
+link.css('cursor', 'pointer');
+link.click(function() {
+    overlay.toggle();
 });
 
- 
-$(function() {
-    var link = $('#regLink');
-    $('#regForm').hide();
-    link.removeAttr('href');
-    link.css('cursor', 'pointer');
-    link.click(function() {
-        $overlay.toggle();
-        $('#regForm').show();
-    });
-
+    
 });
